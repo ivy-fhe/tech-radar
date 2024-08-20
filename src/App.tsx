9e1b002b6d7848d718ed03be2def983433fa3df9
Fabian Heuberger <fabian.heuberger@axonivy.com>
2024-08-20 21:08:24 +0200

    removed escape listener

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 
 import { TechRadar } from './components/TechRadar'
 
 function App() {
-  const [count, setCount] = useState(0)
-  window.addEventListener('keyup', (event) => {
-    let elements = document.getElementsByClassName('expanded');
-    for(let i = 0; i < elements.length; i++){
-      if(elements[i]){
-        elements[i].classList.remove('expanded');
-        elements[i].classList.add('collapsed');
-      }
-      
-    }
-  });
   return (
-    
     <div className='content'>
       <TechRadar/>
     </div>
--- src/components/Quadrant.tsx
+++ src/components/Quadrant.tsx
@@ -1,7 +1,7 @@
 import { SectorName, Sector} from './Sector';
 import './Quadrant.css';
 import { Point } from './TechRadar';
-import { useState } from 'react';
+import { ReactNode, useState } from 'react';
 
 export enum Category {
     Techniques,
@@ -31,6 +31,22 @@ enum ExpState {
 export const Quadrant = ({cat, quadrantItems, callback} : {cat: Category, quadrantItems: QuadrantItems, callback: (text: string) => void}) => {
     let qClass;
     const [expStatus, setExpStatus] = useState(ExpState[ExpState.collapsed]);
+    const itemList: Array<ReactNode> = [];
+    itemList.push(<h1>Hold</h1>)
+    let list = <ul>{quadrantItems.hold.map(e => <li>{e.name + " " + e.description}</li>)}</ul>
+    itemList.push(list);
+
+    itemList.push(<h1>Trial</h1>)
+    list = <ul>{quadrantItems.trial.map(e => <li>{e.name + " " + e.description}</li>)}</ul>
+    itemList.push(list);
+
+    itemList.push(<h1>Specific</h1>)
+    list = <ul>{quadrantItems.specific.map(e => <li>{e.name + " " + e.description}</li>)}</ul>
+    itemList.push(list);
+
+    itemList.push(<h1>Adopt</h1>)
+    list = <ul>{quadrantItems.adopt.map(e => <li>{e.name + " " + e.description}</li>)}</ul>
+    itemList.push(list);
 
     switch(cat) {
         case Category.Techniques:
@@ -48,8 +64,11 @@ export const Quadrant = ({cat, quadrantItems, callback} : {cat: Category, quadra
     }
     return (
         <>
-            
             <div className={"quadrant " + expStatus + qClass} onClick={() => setExpStatus(ExpState[ExpState.expanded])}>
+                <div className='itemListWrapper'>
+                    {itemList}
+                </div>
+                <div className='itemList'></div>
                 <p className='quadrantTitle'>{CATEGORY_LOOKUP.get(cat)}</p>
                 <button className='closeBtn' type='submit' onClick={(event) => {
                     event.stopPropagation();
