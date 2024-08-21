a8f7e9b8826180ba217e145bacf4eceb414aac51
Fabian Heuberger <fabian.heuberger@axonivy.com>
2024-08-21 14:45:01 +0200

    fixed list in expanded view

---
+++ src/util/Highlight.ts
@@ -0,0 +1,14 @@
+import { Point } from "../components/TechRadar";
+
+export const handleHighlight = (element: Point, remove: boolean, callback: (str: string) => void) => {
+    let htmlElements = document.getElementsByClassName(element.id);
+    for(let i = 0; i < htmlElements.length; i++){
+        if(remove){
+            htmlElements[i].classList.remove('highlight');
+            callback("");
+        }else{
+            htmlElements[i].classList.add('highlight');
+            callback(element.description);
+        }
+    }
+}
