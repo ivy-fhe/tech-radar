578331b37944e2c440509a42e5745c0a79d614ef
Fabian Heuberger <fabian.heuberger@axonivy.com>
2024-08-21 16:55:12 +0200

    github language fetching-service

---
+++ src/util/GithubLanguages.ts
@@ -0,0 +1,22 @@
+import { GITHUB_TOKEN } from "../secrets/github";
+
+export const getLang = async () => {
+    let owner = 'ivy-fhe';
+    let repo = 'tech-radar';
+    
+    return await fetchLanguages(owner, repo, GITHUB_TOKEN);
+}
+
+const fetchLanguages = async (owner: string, repo: string, token: string) => {
+    const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/languages`, {
+      method: 'GET',
+      headers: {
+        "Accept": "application/vnd.github+json",
+        "Authorization": "Bearer "+ token,
+        "X-GitHub-Api-Version": "2022-11-28"
+      }
+    });
+    let resp = await response.json();
+    console.log(resp);
+    return resp;
+  }
